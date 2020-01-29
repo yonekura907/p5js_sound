6eb71ea37eebfa1202df92a5a789c1d6f5f330ee
akio.yonekura <yonekura907@gmail.com>
2020-01-29 21:52:14 +0900

    up

--- docs/js/sequencer.js
+++ docs/js/sequencer.js
@@ -1,5 +1,5 @@
 class Sequencer {
-    constructor(aY,aMidi) {
+    constructor(aY, aMidi) {
         this.y = aY;
         this.r = 40;
         this.x = [];
@@ -15,7 +15,7 @@ class Sequencer {
         this.releaseLevel = 0; // 消えるレベル
 
         // シーケンス用配列
-        this.sqArr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        this.sqArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         // ループカウント
         this.loopCount = 0;
@@ -23,14 +23,14 @@ class Sequencer {
 
     }
 
-    init(){
+    init() {
         // オシレーターの生成
         this.osc = new p5.Oscillator('sine');
         this.osc.amp(0);
         this.osc.start();
         this.osc.freq(this.freqValue);
 
-        this.env = new p5.Env();
+        this.env = new p5.Envelope();
         this.env.setADSR(this.attackTime, this.decayTime, this.susPercent, this.releaseTime);
         this.env.setRange(this.attackLevel, this.releaseLevel);
 
@@ -41,9 +41,9 @@ class Sequencer {
             this.btnArr[i] = createDiv(i); //<button>の生成
             this.btnArr[i].id(i); //<button>のvalueにi番を設定
             this.btnArr[i].class('btn');
-            this.btnArr[i].position(50*i + 30, this.y - 20);
-            this.btnArr[i].mousePressed(function(evt){
-                if( thatSqArr[this.elt.id] == 0 ){
+            this.btnArr[i].position(50 * i + 30, this.y - 20);
+            this.btnArr[i].mousePressed(function (evt) {
+                if (thatSqArr[this.elt.id] == 0) {
                     // トグル 現状の値が0なら1を設定
                     thatSqArr[this.elt.id] = 1;
                 } else {
@@ -52,18 +52,18 @@ class Sequencer {
                 }
                 return false;
             });
-            this.x[i] = 50*i + 50;
+            this.x[i] = 50 * i + 50;
         }
 
     }
 
-    draw(){
+    draw() {
         // シーケンサー用の円の配置
         for (var i = 0; i < this.sqArr.length; i++) {
             noStroke(); //線なし
 
             // シーケンス用配列の値が0なら
-            if(this.sqArr[i] == 0){
+            if (this.sqArr[i] == 0) {
                 fill(50); // 塗りはグレー
             } else {
                 // シーケンス用配列の値が1なら
@@ -72,7 +72,7 @@ class Sequencer {
             // 円の配置
             ellipse(this.x[i], this.y, this.r);
 
-            if(i == this.loopCount){
+            if (i == this.loopCount) {
                 // ループ用の円の色
                 fill(255, 0, 0, 70);
                 // ループ用の円の配置
@@ -81,18 +81,18 @@ class Sequencer {
         }
 
 
-        if(frameCount % 4 === 0){
+        if (frameCount % 4 === 0) {
             // ループカンターをインクリメント
             this.loopCount++;
         }
 
         // ループカウンターの数がシーケンス用配列の最大になったら
-        if(this.loopCount >= this.sqArr.length){
+        if (this.loopCount >= this.sqArr.length) {
             this.loopCount = 0; //ループカンターを0に戻す
         }
 
         // シーケンス用配列のループカンター番目の値が1なら
-        if(this.sqArr[this.loopCount] !== 0){
+        if (this.sqArr[this.loopCount] !== 0) {
             this.env.play(this.osc, 0, this.sustainTime);
             this.show(this.loopCount);
         }
@@ -107,17 +107,17 @@ class Sequencer {
         beginShape();
         for (var i = 0; i < waveform.length; i++) {
             x = map(i, 0, waveform.length, 0, width);
-            y = map(waveform[i], -1.0, 1.0, windowHeight - 100, height/2);
+            y = map(waveform[i], -1.0, 1.0, windowHeight - 100, height / 2);
             vertex(x, y);
         }
         endShape();
     }
 
-    show(count){
+    show(count) {
         // console.log('show' + count);
 
-        if(frameCount % 4 === 0){
-            circles.push(new Circle(this.y,count));
+        if (frameCount % 4 === 0) {
+            circles.push(new Circle(this.y, count));
         }
 
     }
